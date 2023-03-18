fbddab20bf40e99c3776a2fe76999ab8e7b07e74
naim <96814060+naim514@users.noreply.github.com>
2023-03-18 15:30:49 +0100

    Mise à jour main.blocks, main.ts, custom.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+ESP8266_IoT_naim.on_HTTP_recevid(function (HTTP_Status_Code, Data) {
+    basic.showString("hhh")
+})
+ESP8266_IoT_naim.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+ESP8266_IoT_naim.connectWifi("Ooredoo 4G_DE143A", "21554265")
+ESP8266_IoT_naim.connectThingSpeak()
 basic.forever(function () {
 	
 })
